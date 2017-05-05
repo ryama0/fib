e1e3d38e3046715a15eb2d7cfec80a52595af942
ryama <create.ryama0@gmail.com>
2017-05-05 18:18:22 +0900

    Add source

---
+++ src/main/scala/Fib.scala
@@ -0,0 +1,17 @@
+package ryama0
+
+object Fib {
+  def calculate(num: Integer): Integer = {
+    if (num <= 0)
+      return 0
+    else if (num == 1)
+      return 1
+    else
+      return this.calculate(num - 2) + this.calculate(num - 1)
+
+  }
+
+  def execute(args: String): String = {
+    this.calculate(args.split(",").head.toInt).toString
+  }
+}
